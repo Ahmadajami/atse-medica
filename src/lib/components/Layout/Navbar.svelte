e658465332517055d3a97c96131f4842cfb2cace
Ahmad Alajami <ahmad_2000_aj@hotmail.com>
2025-08-18 14:11:03 +0300

    Logo Added

--- src/lib/components/Layout/Navbar.svelte
+++ src/lib/components/Layout/Navbar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { setLocale } from '$lib/paraglide/runtime';
+	import { localizeHref, setLocale } from '$lib/paraglide/runtime';
 	import Globe from '@lucide/svelte/icons/globe';
 
 	import BrandName from '$lib/components/Layout/BrandName.svelte';
 
 	import Hamburger from '$lib/components/Layout/Hamburger.svelte';
+	import logo from '$lib/assets/atse-logo.png?enhanced';
 	let { isArabic = $bindable() } = $props();
 	function translate(word: string, t: string) {
 		return isArabic ? t : word;
@@ -13,9 +14,10 @@
 
 <nav class="bg-base-100/80 fixed top-0 right-0 left-0 z-40 h-16 w-full backdrop-blur-sm">
 	<div class="mx-auto flex h-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
-		<div class="flex items-center space-x-2">
+		<a href={localizeHref('/')} class="flex items-center space-x-2">
+			<enhanced:img src={logo} width="40" height="40" class="inline-flex" alt="Atse Medica Logo" />
 			<BrandName bind:isArabic />
-		</div>
+		</a>
 
 		<ul class="hidden space-x-8 font-medium md:flex">
 			<li><a href="#home" class="hover:text-primary">{translate('Home', 'الرئيسية')}</a></li>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+	import BrandName from '$lib/components/Layout/BrandName.svelte';
+	import Instagram from '@lucide/svelte/icons/instagram';
+	import Facebook from '@lucide/svelte/icons/facebook';
+	import Globe from '@lucide/svelte/icons/globe';
+	import logo from '$lib/assets/atse-logo.png?enhanced';
+
+	import type { PageProps } from './$types';
+	import { getLocale } from '$lib/paraglide/runtime';
+
+	let { data }: PageProps = $props();
+	let isArabic = $state(getLocale() == 'ar');
+</script>
+
+<section class="container mx-auto flex min-h-[75vh] items-center justify-center">
+	<article class="mx-auto max-w-7xl">
+		<section class="flex flex-col space-y-8">
+			<div class="flex-col self-center">
+				<enhanced:img
+					src={logo}
+					width="50"
+					height="50"
+					class="inline-flex pb-8"
+					alt="Atse Medica Logo"
+				/>
+				<BrandName className="text-4xl text-wrap" bind:isArabic />
+			</div>
+
+			<div class=" inline-flex space-x-8 self-center">
+				<div>
+					<a href="https://www.instagram.com/atse.medica/" class="inline-flex hover:text-primary"
+						><Instagram /></a
+					>
+				</div>
+				<div>
+					<a
+						href="https://www.facebook.com/people/ATS%C3%89-Medica/61579210582618/"
+						class="inline-flex hover:text-primary"><Facebook /></a
+					>
+				</div>
+				<div>
+					<a href="#" class="inline-flex hover:text-primary"><Globe /></a>
+				</div>
+				<div>
+					<!-- svelte-ignore a11y_consider_explicit_label -->
+					<a href="https://wa.me/963968500077" class="inline-flex hover:text-primary">
+						<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+							><path
+								fill="currentColor"
+								d="M19.05 4.91A9.82 9.82 0 0 0 12.04 2c-5.46 0-9.91 4.45-9.91 9.91c0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21c5.46 0 9.91-4.45 9.91-9.91c0-2.65-1.03-5.14-2.9-7.01m-7.01 15.24c-1.48 0-2.93-.4-4.2-1.15l-.3-.18l-3.12.82l.83-3.04l-.2-.31a8.26 8.26 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24c2.2 0 4.27.86 5.82 2.42a8.18 8.18 0 0 1 2.41 5.83c.02 4.54-3.68 8.23-8.22 8.23m4.52-6.16c-.25-.12-1.47-.72-1.69-.81c-.23-.08-.39-.12-.56.12c-.17.25-.64.81-.78.97c-.14.17-.29.19-.54.06c-.25-.12-1.05-.39-1.99-1.23c-.74-.66-1.23-1.47-1.38-1.72c-.14-.25-.02-.38.11-.51c.11-.11.25-.29.37-.43s.17-.25.25-.41c.08-.17.04-.31-.02-.43s-.56-1.34-.76-1.84c-.2-.48-.41-.42-.56-.43h-.48c-.17 0-.43.06-.66.31c-.22.25-.86.85-.86 2.07s.89 2.4 1.01 2.56c.12.17 1.75 2.67 4.23 3.74c.59.26 1.05.41 1.41.52c.59.19 1.13.16 1.56.1c.48-.07 1.47-.6 1.67-1.18c.21-.58.21-1.07.14-1.18s-.22-.16-.47-.28"
+							/></svg
+						></a
+					>
+				</div>
+			</div>
+		</section>
+	</article>
+</section>
